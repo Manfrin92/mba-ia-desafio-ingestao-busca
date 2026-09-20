9729f5ea3bd228f5a72029965676885893fe7743
Manfrin <manfredd_10@hotmail.com>
2026-09-20 10:46:35 -0300

    feat: ingest and insert into db

--- src/ingest.py
+++ src/ingest.py
@@ -3,33 +3,44 @@ from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
-from langchain_openai import OpenAIEmbeddings
-
-load_dotenv()
-
-PDF_PATH = os.getenv("PDF_PATH")
-
-loader = PyPDFLoader(PDF_PATH)
-docs = loader.load()
-
-splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
-
-chunks = splitter.split_documents(docs)
-
-enriched = [
-    Document(
-        page_content=d.page_content,
-        metadata={k: v for k, v in d.metadata.items() if v not in ("", None)}
-    )
-    for d in chunks
-]
-
-ids = [f"doc-{i}" for i in range(len(enriched))]
-
-embeddings = OpenAIEmbeddings(model=os.getenv("OPENAI_MODEL","text-embedding-3-small"))
+from langchain_google_genai import GoogleGenerativeAIEmbeddings
+from langchain_postgres import PGVector
 
 def ingest_pdf():
-    pass
+    load_dotenv()
+
+    PDF_PATH = os.getenv("PDF_PATH")
+
+    loader = PyPDFLoader(PDF_PATH)
+    docs = loader.load()
+
+    splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
+
+    chunks = splitter.split_documents(docs)
+
+    enriched = [
+        Document(
+            page_content=d.page_content,
+            metadata={k: v for k, v in d.metadata.items() if v not in ("", None)}
+        )
+        for d in chunks
+    ]
+
+    ids = [f"doc-{i}" for i in range(len(enriched))]
+
+    embeddings = GoogleGenerativeAIEmbeddings(
+        model=os.getenv("GOOGLE_EMBEDDING_MODEL"),
+        google_api_key=os.getenv("GOOGLE_API_KEY"),
+    )
+
+    store = PGVector(
+        embeddings=embeddings,
+        collection_name=os.getenv("PG_VECTOR_COLLECTION_NAME"),
+        connection=os.getenv("DATABASE_URL"),
+        use_jsonb=True,
+    )
+
+    store.add_documents(documents=enriched, ids=ids)
 
 if __name__ == "__main__":
     ingest_pdf()
