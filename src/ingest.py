cf7bb72afff0f30219bb2bd8fc598fe23b5949ae
Manfrin <manfredd_10@hotmail.com>
2026-09-20 09:55:47 -0300

    feat: openapi

--- src/ingest.py
+++ src/ingest.py
@@ -3,6 +3,7 @@ from dotenv import load_dotenv
 from langchain_community.document_loaders import PyPDFLoader
 from langchain_text_splitters import RecursiveCharacterTextSplitter
 from langchain_core.documents import Document
+from langchain_openai import OpenAIEmbeddings
 
 load_dotenv()
 
@@ -21,7 +22,11 @@ enriched = [
         metadata={k: v for k, v in d.metadata.items() if v not in ("", None)}
     )
     for d in chunks
-]  
+]
+
+ids = [f"doc-{i}" for i in range(len(enriched))]
+
+embeddings = OpenAIEmbeddings(model=os.getenv("OPENAI_MODEL","text-embedding-3-small"))
 
 def ingest_pdf():
     pass
