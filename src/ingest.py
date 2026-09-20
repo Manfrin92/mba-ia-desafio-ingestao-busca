f431d18cbffe5507adf563a5de9029ae066805b5
Manfrin <manfredd_10@hotmail.com>
2026-09-20 09:35:30 -0300

    feat: read pdf and chunk it

--- src/ingest.py
+++ src/ingest.py
@@ -1,9 +1,18 @@
 import os
 from dotenv import load_dotenv
+from langchain_community.document_loaders import PyPDFLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 load_dotenv()
 
 PDF_PATH = os.getenv("PDF_PATH")
+
+loader = PyPDFLoader(PDF_PATH)
+docs = loader.load()
+
+splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=150)
+
+chunks = splitter.split_documents(docs)
 
 def ingest_pdf():
     pass
